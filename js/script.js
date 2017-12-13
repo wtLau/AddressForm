94a45fcfadc6c515656b3c19af871938d6a70f1f
Brian Lau <lauwangtatbrian@gmail.com>
2017-12-13 10:06:25 -0800

    add class

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-class addressForm {
 
+class addressForm {
     submitForm() {
-        $('.address-form').on('submit', function () {
+        console.log('hihih');
+        return $('.address-form').on('submit', function () {
             event.preventDefault();
             alert('hi')
         })
     }
+
+    intlTel() {
+        $(document).ready($("#phone").intlTelInput({
+            // allowDropdown: false,
+            // autoHideDialCode: false,
+            autoPlaceholder: "off",
+            // dropdownContainer: "body",
+            // excludeCountries: ["us"],
+            // formatOnDisplay: false,
+            geoIpLookup: function (callback) {
+                $.get("http://ipinfo.io", function () { }, "jsonp").always(function (resp) {
+                    var countryCode = (resp && resp.country) ? resp.country : "";
+                    callback(countryCode);
+                });
+            },
+            // hiddenInput: "full_number",
+            initialCountry: "auto",
+            // nationalMode: false,
+            onlyCountries: ['us', 'gb', 'tw', 'cn', 'ca', 'do', 'hk', 'fr'],
+            // placeholderNumberType: "MOBILE",
+            // preferredCountries: ['cn', 'jp'],
+            // separateDialCode: true,
+            utilsScript: "./js/utils.js"
+        }));
+    }
 }
+const addressFormCall = new addressForm()
+addressFormCall.submitForm()
+addressFormCall.intlTel()
